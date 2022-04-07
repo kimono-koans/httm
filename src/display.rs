eee6f9e2c395c19518af02516367741bc9dd94cf
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-04-07 14:33:39 -0500

    No more for loops in display

--- src/display.rs
+++ src/display.rs
@@ -45,12 +45,12 @@ fn display_raw(
     let delimiter = if config.opt_zeros { '\0' } else { '\n' };
 
     // so easy!
-    for version in &snaps_and_live_set {
-        for pathdata in version {
+    snaps_and_live_set.iter().for_each(|version| {
+        version.iter().for_each(|pathdata| {
             let display_path = pathdata.path_buf.display();
             buffer += &format!("{}{}", display_path, delimiter);
-        }
-    }
+        });
+    });
 
     Ok(buffer)
 }
@@ -69,73 +69,76 @@ fn display_pretty(
         write_out_buffer += &format!("{}\n", fancy_border_string);
     }
 
-    for (idx, pathdata_set) in snaps_and_live_set.iter().enumerate() {
-        let mut pathdata_set_buffer = String::new();
+    snaps_and_live_set
+        .iter()
+        .enumerate()
+        .for_each(|(idx, pathdata_set)| {
+            let mut pathdata_set_buffer = String::new();
 
-        for pathdata in pathdata_set {
-            let path_date = display_date(&pathdata.system_time);
-            let path_size;
-            let display_path;
-            let display_padding_size;
-            let display_padding_path;
-            let fixed_width_padding = (0..5).map(|_| " ").collect();
+            pathdata_set.iter().for_each(|pathdata| {
+                let path_date = display_date(&pathdata.system_time);
+                let path_size;
+                let display_path;
+                let display_padding_size;
+                let display_padding_path;
+                let fixed_width_padding = (0..5).map(|_| " ").collect();
 
-            // paint the live string with ls colors
-            let painted_path = if idx == 1 && !config.opt_no_pretty {
-                let path = &pathdata.path_buf;
-                paint_string(path, &path.to_string_lossy())
-            } else {
-                pathdata.path_buf.to_string_lossy().into_owned()
-            };
+                // paint the live string with ls colors
+                let painted_path = if idx == 1 && !config.opt_no_pretty {
+                    let path = &pathdata.path_buf;
+                    paint_string(path, &path.to_string_lossy())
+                } else {
+                    pathdata.path_buf.to_string_lossy().into_owned()
+                };
 
-            // tab delimited if "no pretty", and no border lines
-            if !config.opt_no_pretty {
-                path_size = format!(
-                    "{:>width$}",
-                    display_human_size(pathdata),
-                    width = size_padding
+                // tab delimited if "no pretty", and no border lines
+                if !config.opt_no_pretty {
+                    path_size = format!(
+                        "{:>width$}",
+                        display_human_size(pathdata),
+                        width = size_padding
+                    );
+                    display_path = format!("{:<width$}", painted_path, width = size_padding);
+                    display_padding_size = (0..size_padding).map(|_| " ").collect();
+                    display_padding_path = fixed_width_padding;
+                } else {
+                    path_size = display_human_size(pathdata);
+                    display_path = painted_path;
+                    display_padding_size = "\t".to_string();
+                    display_padding_path = "\t".to_string();
+                }
+
+                // displays blanks for phantom values, equaling their dummy lens and dates.
+                //
+                // see struct PathData for more details as to why we use a dummy instead of
+                // a None value here.
+                let (display_date, display_size) = if !pathdata.is_phantom {
+                    let date = path_date;
+                    let size = path_size;
+                    (date, size)
+                } else {
+                    let date: String = (0..path_date.len()).map(|_| " ").collect();
+                    let size: String = (0..path_size.len()).map(|_| " ").collect();
+                    (date, size)
+                };
+                pathdata_set_buffer += &format!(
+                    "{}{}{}{}\"{}\"\n",
+                    display_date,
+                    display_padding_size,
+                    display_size,
+                    display_padding_path,
+                    display_path
                 );
-                display_path = format!("{:<width$}", painted_path, width = size_padding);
-                display_padding_size = (0..size_padding).map(|_| " ").collect();
-                display_padding_path = fixed_width_padding;
+            });
+            if !config.opt_no_pretty && !pathdata_set_buffer.is_empty() {
+                pathdata_set_buffer += &format!("{}\n", fancy_border_string);
+                write_out_buffer += &pathdata_set_buffer;
             } else {
-                path_size = display_human_size(pathdata);
-                display_path = painted_path;
-                display_padding_size = "\t".to_string();
-                display_padding_path = "\t".to_string();
+                pathdata_set_buffer.lines().rev().for_each(|line| {
+                    write_out_buffer += &format!("{}\n", line);
+                });
             }
-
-            // displays blanks for phantom values, equaling their dummy lens and dates.
-            //
-            // see struct PathData for more details as to why we use a dummy instead of
-            // a None value here.
-            let (display_date, display_size) = if !pathdata.is_phantom {
-                let date = path_date;
-                let size = path_size;
-                (date, size)
-            } else {
-                let date: String = (0..path_date.len()).map(|_| " ").collect();
-                let size: String = (0..path_size.len()).map(|_| " ").collect();
-                (date, size)
-            };
-            pathdata_set_buffer += &format!(
-                "{}{}{}{}\"{}\"\n",
-                display_date,
-                display_padding_size,
-                display_size,
-                display_padding_path,
-                display_path
-            );
-        }
-        if !config.opt_no_pretty && !pathdata_set_buffer.is_empty() {
-            pathdata_set_buffer += &format!("{}\n", fancy_border_string);
-            write_out_buffer += &pathdata_set_buffer;
-        } else {
-            for line in pathdata_set_buffer.lines().rev() {
-                write_out_buffer += &format!("{}\n", line);
-            }
-        }
-    }
+        });
 
     Ok(write_out_buffer)
 }
@@ -145,8 +148,8 @@ fn calculate_padding(snaps_and_live_set: &[Vec<PathData>]) -> (usize, String) {
     let mut fancy_border = 1usize;
 
     // calculate padding and borders for display later
-    for ver_set in snaps_and_live_set {
-        for pathdata in ver_set {
+    snaps_and_live_set.iter().for_each(|ver_set| {
+        ver_set.iter().for_each(|pathdata| {
             let display_date = display_date(&pathdata.system_time);
             let display_size = format!(
                 "{:>width$}",
@@ -163,8 +166,8 @@ fn calculate_padding(snaps_and_live_set: &[Vec<PathData>]) -> (usize, String) {
 
             size_padding = display_size_len.max(size_padding);
             fancy_border = formatted_line_len.max(fancy_border);
-        }
-    }
+        });
+    });
 
     // has to be a more idiomatic way to do this
     // if you know, let me know
