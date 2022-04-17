8a86d1b8f335f90b62fef95cc655ffb88517ad35
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-04-17 17:13:11 -0500

    Restructure, bump version

--- src/httm.rs
+++ src/httm.rs
@@ -17,11 +17,13 @@
 
 mod deleted;
 mod display;
+mod helper;
 mod interactive;
 mod lookup;
 
 use crate::deleted::deleted_exec;
 use crate::display::display_exec;
+use crate::helper::{install_hot_keys, list_all_filesystems, read_stdin};
 use crate::interactive::interactive_exec;
 use crate::lookup::lookup_exec;
 
@@ -31,10 +33,9 @@ use rayon::prelude::*;
 use std::{
     error::Error,
     fmt,
-    fs::{DirEntry, Metadata, OpenOptions},
-    io::{BufRead, Read, Write},
+    fs::{DirEntry, Metadata},
+    io::Write,
     path::{Path, PathBuf},
-    process::Command as ExecProcess,
     time::SystemTime,
 };
 use which::which;
@@ -445,7 +446,7 @@ fn parse_args() -> ArgMatches {
     clap::Command::new("httm")
         .about("\nBy default, httm will display non-interactive information about unique file versions contained on ZFS snapshots.\n\n\
         You may also select from the various interactive modes below to browse for, select, and/or restore files.")
-        .version("0.7.1") 
+        .version("0.7.5") 
         .arg(
             Arg::new("INPUT_FILES")
                 .help("in the default, non-interactive mode, put requested files here.  If you enter no files, \
@@ -584,164 +585,4 @@ fn exec() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     out.flush()?;
 
     Ok(())
-}
-
-fn read_stdin() -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let mut buffer = String::new();
-    let stdin = std::io::stdin();
-    let mut stdin = stdin.lock();
-    stdin.read_line(&mut buffer)?;
-
-    let broken_string: Vec<String> = buffer
-        .split_ascii_whitespace()
-        .into_iter()
-        .map(|i| i.to_owned())
-        .collect();
-
-    Ok(broken_string)
-}
-
-fn install_hot_keys() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    // get our home directory
-    let home_dir = if let Ok(home) = std::env::var("HOME") {
-        if let Ok(path) = PathBuf::from(&home).canonicalize() {
-            path
-        } else {
-            return Err(HttmError::new(
-                "$HOME, as set in your environment, does not appear to exist",
-            )
-            .into());
-        }
-    } else {
-        return Err(HttmError::new("$HOME does not appear to be set in your environment").into());
-    };
-
-    // check whether httm-key-bindings.zsh is already sourced
-    // and, if not, open ~/.zshrc append only for sourcing the httm-key-bindings.zsh
-    let mut buffer = String::new();
-    let zshrc_path: PathBuf = [&home_dir, &PathBuf::from(".zshrc")].iter().collect();
-    let mut zshrc_file = if let Ok(file) = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .append(true)
-        .open(zshrc_path)
-    {
-        file
-    } else {
-        return Err(HttmError::new(
-                "Either your ~/.zshrc file does not exist or you do not have the permissions to access it.",
-            )
-            .into());
-    };
-    zshrc_file.read_to_string(&mut buffer)?;
-
-    // check that there are not lines in the zshrc that contain "source" and "httm-key-bindings.zsh"
-    if !buffer
-        .lines()
-        .filter(|line| !line.starts_with('#'))
-        .any(|line| line.contains("source") && line.contains("httm-key-bindings.zsh"))
-    {
-        // create key binding file -- done at compile time
-        let zsh_hot_key_script = include_str!("../scripts/httm-key-bindings.zsh");
-        let zsh_script_path: PathBuf = [&home_dir, &PathBuf::from(".httm-key-bindings.zsh")]
-            .iter()
-            .collect();
-        // creates script file in user's home dir or will fail if file already exists
-        if let Ok(mut zsh_script_file) = OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open(zsh_script_path)
-        {
-            zsh_script_file.write_all(zsh_hot_key_script.as_bytes())?;
-        } else {
-            eprintln!("httm: .httm-key-bindings.zsh is already present in user's home directory.");
-        }
-
-        // append "source ~/.httm-key-bindings.zsh" to zshrc
-        zshrc_file.write_all(
-            "\n# httm: zsh hot keys script\nsource ~/.httm-key-bindings.zsh\n".as_bytes(),
-        )?;
-        eprintln!("httm: zsh hot keys were installed successfully.");
-    } else {
-        eprintln!(
-            "httm: zsh hot keys appear to already be sourced in the user's ~/.zshrc. Quitting."
-        );
-    }
-
-    std::process::exit(0)
-}
-
-fn list_all_filesystems(
-    shell_command: String,
-    zfs_command: String,
-    mount_command: String,
-) -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync + 'static>> {
-    // build zfs query to execute - in case the fast path fails
-    // this is slower but we are sure it works everywhere
-    let fallback = |shell_command: String, zfs_command: String| {
-        let exec_command = zfs_command + " list -H -t filesystem -o mountpoint,mounted";
-
-        let command_output = std::str::from_utf8(
-            &ExecProcess::new(&shell_command)
-                .arg("-c")
-                .arg(exec_command)
-                .output()?
-                .stdout,
-        )?
-        .to_owned();
-
-        let res = command_output
-            .par_lines()
-            .filter(|line| line.contains("yes"))
-            .filter_map(|line| line.split('\t').next())
-            // sanity check: does the filesystem exists, if not, filter it
-            .filter(|line| Path::new(line).exists())
-            .map(|line| line.to_owned())
-            .collect::<Vec<String>>();
-        Ok(res)
-    };
-
-    // read datasets from `mount` if possible -- this is much faster than using zfs command
-    // reading /proc/mounts is even faster but Linux only (this should work on the BSDs and MacOS)
-    let exec_command = mount_command + " -t zfs";
-
-    let command_output = std::str::from_utf8(
-        &ExecProcess::new(&shell_command)
-            .arg("-c")
-            .arg(exec_command)
-            .output()?
-            .stdout,
-    )?
-    .to_owned();
-
-    if command_output.is_empty() {
-        fallback(shell_command, zfs_command)
-    } else {
-        // parse "mount -t zfs" for filesystems
-        let res: Vec<String> = command_output
-            .par_lines()
-            .map(|line| line.split_once(&"on "))
-            .flatten()
-            .map(|(_,last)| last)
-            .map(|line|
-                // GNU Linux output
-                if line.contains("type") {
-                    line.split_once(&" type")
-                // Busybox and BSD output
-                } else {
-                    line.split_once(&" (")
-                })
-            .flatten()
-            .map(|(first,_)| first)
-            // sanity check does the filesystem exists, if not, filter it
-            .filter(|line| Path::new(line).exists())
-            .map(|line| line.to_owned())
-            .collect();
-
-        if res.is_empty() {
-            fallback(shell_command, zfs_command)
-        } else {
-            Ok(res)
-        }
-    }
 }
