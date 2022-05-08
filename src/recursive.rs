63315f4e467667f922eaedd0630ab67dbb7086f6
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-05-08 08:35:08 -0500

    Working

--- src/recursive.rs
+++ src/recursive.rs
@@ -19,7 +19,7 @@ use crate::deleted::get_deleted;
 use crate::display::display_exec;
 use crate::interactive::SelectionCandidate;
 use crate::utility::httm_is_dir;
-use crate::{Config, DeletedMode, ExecMode, PathData};
+use crate::{Config, DeletedMode, ExecMode, PathData, HttmError};
 
 use rayon::{iter::Either, prelude::*};
 use skim::prelude::*;
@@ -102,11 +102,7 @@ pub fn enumerate_directory(
 
                 // thread spawn fn enumerate_directory - permits recursion into dirs without blocking
                 let handle = std::thread::spawn(move || {
-                    let _ = get_pseudo_live_versions(
-                        config_clone,
-                        &requested_dir_clone,
-                        &tx_item_clone,
-                    );
+                    let _ = enumerate_deleted(config_clone, &requested_dir_clone, &tx_item_clone);
                 });
                 join_handles.push(handle);
             };
@@ -159,6 +155,8 @@ pub fn enumerate_directory(
     }
 
     // here we make sure to wait until all child threads have exited before returning
+    // this allows the main work of the fn to keep running while while work on deleted
+    // files in the background
     if !join_handles.is_empty() {
         let _ = join_handles
             .into_iter()
@@ -172,35 +170,117 @@ pub fn enumerate_directory(
 // which have been found on snapshots, we return to the user "the path that
 // once was" in their browse panel
 //
-// why another fn? so we can spawn another thread and not block a finding deleted files
-// generally takes a long time.
-fn get_pseudo_live_versions(
+// why another fn? so we can spawn another thread and not block on finding deleted files
+// which generally takes a long time.
+fn enumerate_deleted(
     config: Arc<Config>,
     requested_dir: &Path,
     tx_item: &SkimItemSender,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let pseudo_live_versions: Vec<PathBuf> = get_deleted(&config, requested_dir)?
-        .par_iter()
+    let deleted = get_deleted(&config, requested_dir)?;
+
+    let (vec_dirs, vec_files): (Vec<PathData>, Vec<PathData>) =
+        deleted.into_par_iter().partition_map(|pathdata| {
+            if pathdata.is_dir() {
+                Either::Left(pathdata)
+            } else {
+                Either::Right(pathdata)
+            }
+        });
+
+    let behind_deleted_dirs: Vec<PathBuf> = vec_dirs
+        .clone()
+        .into_par_iter()
+        .map(|pathdata| pathdata.path_buf)
+        .map(|deleted_dir| behind_deleted_dir(&deleted_dir, &requested_dir).unwrap())
+        .flatten()
+        .collect();
+
+    let pseudo_live_versions: Vec<PathBuf> = [&vec_dirs, &vec_files]
+        .into_par_iter()
+        .flatten()
         .map(|path| path.path_buf.file_name())
         .flatten()
         .map(|file_name| requested_dir.join(file_name))
         .collect();
 
-    pseudo_live_versions.iter().for_each(|path| {
-        let _ = tx_item.send(Arc::new(SelectionCandidate::new(
-            config.clone(),
-            path.to_path_buf(),
-        )));
-    });
+    [pseudo_live_versions, behind_deleted_dirs]
+        .iter()
+        .flatten()
+        .for_each(|path| {
+            let _ = tx_item.send(Arc::new(SelectionCandidate::new(
+                config.clone(),
+                path.to_path_buf(),
+            )));
+        });
 
     Ok(())
+}
+
+fn behind_deleted_dir(
+    deleted_dir: &Path,
+    requested_dir: &Path,
+) -> Result<Vec<PathBuf>, Box<dyn std::error::Error + Send + Sync + 'static>> {
+    fn recurse_behind_deleted_dir(
+        dir_name: &Path,
+        from_deleted_dir: &Path,
+        from_requested_dir: &Path,
+        vec_behind_deleted_dirs: &mut Vec<PathBuf>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+
+        let deleted_dir_on_snap = [&from_deleted_dir, &dir_name]
+            .iter()
+            .collect::<PathBuf>();
+
+        let pseudo_live_dir = [&from_requested_dir, &dir_name]
+            .iter()
+            .collect::<PathBuf>();
+
+        let (vec_dirs, vec_files): (Vec<PathBuf>, Vec<PathBuf>) = read_dir(&deleted_dir_on_snap)?
+            .flatten()
+            .par_bridge()
+            .partition_map(|dir_entry| {
+                let path_buf = dir_entry.path();
+                if httm_is_dir(&dir_entry) {
+                    Either::Left(path_buf)
+                } else {
+                    Either::Right(path_buf)
+                }
+            });
+
+        let pseudo_live_versions: Vec<PathBuf> = [&vec_files, &vec_dirs]
+            .into_par_iter()
+            .flatten()
+            .map(|path| path.file_name())
+            .flatten()
+            .map(|file_name| pseudo_live_dir.join(file_name))
+            .collect();
+
+        pseudo_live_versions.into_iter().for_each(|deleted_file| {
+            vec_behind_deleted_dirs.push(deleted_file);
+        });
+
+        vec_dirs.into_iter().for_each(|dir| {
+            let _ = recurse_behind_deleted_dir(&dir, &deleted_dir_on_snap, &pseudo_live_dir, vec_behind_deleted_dirs);
+        });
+
+        Ok(())
+    }
+
+    let mut vec_behind_deleted_dirs = Vec::new();
+    match &deleted_dir.file_name() {
+        Some(dir_name) => recurse_behind_deleted_dir(Path::new(dir_name), deleted_dir.parent().unwrap(), requested_dir, &mut vec_behind_deleted_dirs)?,
+        None => return Err(HttmError::new("Not a valid file!").into()),
+    }
+
+    Ok(vec_behind_deleted_dirs)
 }
 
 fn print_deleted_recursive(
     config: Arc<Config>,
     requested_dir: &Path,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let vec_deleted = get_deleted(&config, requested_dir)?;
+    let mut vec_deleted = get_deleted(&config, requested_dir)?;
     if vec_deleted.is_empty() {
         // Shows progress, while we are finding no deleted files
         if config.opt_recursive {
@@ -223,6 +303,8 @@ fn print_deleted_recursive(
         } else {
             Vec::new()
         };
+
+        vec_deleted.par_sort_unstable_by_key(|pathdata| pathdata.path_buf.clone());
 
         let output_buf = display_exec(&config, [vec_deleted, pseudo_live_versions])?;
         // have to get a line break here, but shouldn't look unnatural
