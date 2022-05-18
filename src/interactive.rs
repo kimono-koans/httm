7b026865ebbe4df9c337cc76143db7572364b055
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-05-17 23:43:08 -0500

    Reorg

--- src/interactive.rs
+++ src/interactive.rs
@@ -15,8 +15,7 @@
 // For the full copyright and license information, please view the LICENSE file
 // that was distributed with this source code.
 
-use std::fs::FileType;
-use std::{ffi::OsString, io::Cursor, path::Path, path::PathBuf, thread, vec};
+use std::{ffi::OsString, fs::FileType, io::Cursor, path::Path, path::PathBuf, thread, vec};
 
 extern crate skim;
 
