8f2dec0ad6544c161826dc8db3b63356e554983f
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-10-30 23:00:06 -0500

    Cleanup

--- scripts/bowie.bash
+++ scripts/bowie.bash
@@ -10,7 +10,6 @@ bowie $(httm --version | cut -f2 -d' ')
 	exit 0
 }
 
-
 print_usage() {
 	local bowie="\e[31mbowie\e[0m"
 	local httm="\e[31mhttm\e[0m"
@@ -48,7 +47,7 @@ show_all_changes() {
 	local previous_version=""
 
 	for current_version in $(httm -n --omit-ditto "$filename"); do
-	# check if initial "previous_version" needs to be set
+		# check if initial "previous_version" needs to be set
 		if [[ -z "$previous_version" ]]; then
 			previous_version="$current_version"
 			continue
@@ -56,7 +55,7 @@ show_all_changes() {
 
 		display_diff "$previous_version" "$current_version"
 
-    		# set current_version to previous_version
+		# set current_version to previous_version
 		previous_version="$current_version"
 	done
 }
@@ -64,8 +63,8 @@ show_all_changes() {
 show_last_change() {
 	local current_version="$1"
 	local previous_version=""
-	
-	previous_version="$( httm --omit-ditto --last-snap --raw "$current_version" )"
+
+	previous_version="$(httm --omit-ditto --last-snap --raw "$current_version")"
 
 	display_diff "$previous_version" "$current_version"
 }
@@ -75,26 +74,32 @@ display_diff() {
 	local current_version="$2"
 
 	# print that current version and previous version differ
-	( diff -q "$previous_version" "$current_version" || true )
+	(diff -q "$previous_version" "$current_version" || true)
 	# print the difference between that current version and previous_version
-	( diff -T "$previous_version" "$current_version" || true )
+	(diff -T "$previous_version" "$current_version" || true)
 }
 
 exec_main() {
 	local all_mode=false
 
+	[[ $# -ge 1 ]] || print_usage
 	[[ "$1" != "-h" && "$1" != "--help" ]] || print_usage
 	[[ "$1" != "-V" && "$1" != "--version" ]] || print_version
-	[[ "$1" != "--last" ]] || shift
-	[[ "$1" != "--all" ]] || ( all_mode=true; shift )
+
+	if [[ $1 == "--all" ]]; then
+		all_mode=true
+		shift
+	elif [[ $1 == "--last" ]]; then
+		shift
+	fi
 
 	for a; do
-		[[ "$1" != -* && "$1" != --* ]] || \
+		[[ "$a" != -* && "$a" != --* ]] ||
 			print_err_exit "Option specified either was not expected or is not permitted in this context."
-		
+
 		canonical_path="$(readlink -e "$a" 2>/dev/null)"
 
-        [[ -n "$canonical_path" ]]  || [[ -e "$canonical_path" ]] || [[ $? -eq 0 ]] || \
+		[[ -n "$canonical_path" ]] || [[ -e "$canonical_path" ]] || [[ $? -eq 0 ]] ||
 			print_err_exit "Could not determine canonical path for: $a"
 
 		if $all_mode; then
@@ -102,7 +107,7 @@ exec_main() {
 		else
 			show_last_change "$canonical_path"
 		fi
-    done
+	done
 }
 
 exec_main "$@"
