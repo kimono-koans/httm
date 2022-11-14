f734e7142419c447915475099fccf3906cd8e640
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-11-14 12:58:47 -0600

    Fix bowie script

--- scripts/bowie.bash
+++ scripts/bowie.bash
@@ -95,7 +95,7 @@ show_all_changes() {
 		print_err "No previous version available for: $filename"
 		return 0
 	elif [[ ${#all_versions[@]} -eq 1 ]]; then
-		show_last_change "$filename"
+		show_single_change "$filename"
 		return 0
 	fi
 
