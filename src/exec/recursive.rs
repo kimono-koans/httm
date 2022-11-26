55721434fe65ceee944e9ebcc05de46348c3c55b
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-11-25 23:27:37 -0600

    Set deleted thread priority

--- src/exec/recursive.rs
+++ src/exec/recursive.rs
@@ -83,11 +83,23 @@ pub fn recursive_exec(
             .unwrap()
     };
 
+    let set_deleted_search_priority = |i| {
+        #[cfg(target_os = "linux")]
+        unsafe {
+            libc::setpriority(i as u32, 0u32, 5i32);
+        }
+        #[cfg(target_os = "macos")]
+        unsafe {
+            libc::setpriority(i as i32, 0u32, 5i32);
+        }
+    };
+
     // build thread pool with a stack size large enough to avoid a stack overflow
     // this will be our one threadpool for directory enumeration ops
     let pool: ThreadPool = rayon::ThreadPoolBuilder::new()
         .stack_size(DEFAULT_STACK_SIZE)
         .num_threads(num_threads)
+        .start_handler(set_deleted_search_priority)  
         .build()
         .expect("Could not initialize rayon threadpool for recursive deleted search");
 
