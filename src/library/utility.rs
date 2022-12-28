38f4bdcd86ccb670e2c8afe88213ac5845f45dc2
electricboogie <32370782+electricboogie@users.noreply.github.com>
2022-12-28 14:09:43 -0600

    Cleanup

--- src/library/utility.rs
+++ src/library/utility.rs
@@ -64,7 +64,7 @@ pub fn make_tmp_path(path: &Path) -> PathBuf {
     PathBuf::from(res)
 }
 
-pub fn copy_attributes(src: &Path, dst: &Path) -> HttmResult<()> {
+fn copy_attributes(src: &Path, dst: &Path) -> HttmResult<()> {
     let src_metadata = src.symlink_metadata()?;
 
     // Mode
