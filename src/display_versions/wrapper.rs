0d949445d84c4933fe69d3eb858867a1ba9e0604
electricboogie <32370782+electricboogie@users.noreply.github.com>
2023-02-12 16:04:12 -0600

    Fix display_config not formatting in preview correctly

--- src/display_versions/wrapper.rs
+++ src/display_versions/wrapper.rs
@@ -37,16 +37,18 @@ impl<'a> std::string::ToString for VersionsDisplayWrapper<'a> {
             ExecMode::NumVersions(num_versions_mode) => {
                 self.map.format_as_num_versions(num_versions_mode)
             }
-            ExecMode::Display if self.config.opt_last_snap.is_some() => {
-                let printable_map = PrintAsMap::from(&self.map);
-                OtherDisplayWrapper::from(self.config, printable_map).to_string()
-            }
-            _ if matches!(
-                self.config.print_mode,
-                PrintMode::FormattedJsonDefault | PrintMode::FormattedJsonNotPretty
-            ) =>
-            {
-                self.map.to_json(self.config)
+            ExecMode::Display => {
+                if self.config.opt_last_snap.is_some() {
+                    let printable_map = PrintAsMap::from(&self.map);
+                    return OtherDisplayWrapper::from(self.config, printable_map).to_string();
+                }
+
+                match self.config.print_mode {
+                    PrintMode::FormattedJsonDefault | PrintMode::FormattedJsonNotPretty => {
+                        self.map.to_json(self.config)
+                    }
+                    _ => self.map.format(self.config),
+                }
             }
             _ => self.map.format(self.config),
         }
