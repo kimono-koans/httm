bb1c0bf506729a5240f6f03e946be54a67569e00
electricboogie <32370782+electricboogie@users.noreply.github.com>
2023-02-27 14:48:53 -0600

    Update copyright info

--- scripts/httm-key-bindings.zsh
+++ scripts/httm-key-bindings.zsh
@@ -10,7 +10,7 @@
 #      /:/  /                                    /:/  /
 #      \/__/                                     \/__/
 #
-# (c) Robert Swinford <robert.swinford<...at...>gmail.com>
+# Copyright (c) 2023, Robert Swinford <robert.swinford<...at...>gmail.com>
 #
 # For the full copyright and license information, please view the LICENSE file
 # that was distributed with this source code.
