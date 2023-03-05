b14937f0d62f21fff6176cfa96772a908a658dd9
electricboogie <32370782+electricboogie@users.noreply.github.com>
2023-03-05 14:29:53 -0600

    Better help

--- src/config/generate.rs
+++ src/config/generate.rs
@@ -222,15 +222,15 @@ fn parse_args() -> ArgMatches {
                 .visible_aliases(&["unique"])
                 .takes_value(true)
                 .default_missing_value("contents")
-                .possible_values(["all", "metadata", "contents"])
+                .possible_values(["all", "no-filter", "metadata", "contents"])
                 .min_values(0)
                 .require_equals(true)
                 .help("comparing file versions solely on the basis of size and modify time (the default \"metadata\" behavior) may return what appear to be \"false positives\", \
                 in the sense that, modify time is not a precise measure of whether a file has actually changed.  A program might overwrite a file with the same contents, \
-                or a user can simply update the modify time via 'touch'.  If only this flag is specified, the \"contents\" option overrides the default \"metadata\" behavior.  \
-                The \"contents\" option can be expensive, as the file versions need to be read back and compared, and should probably only be used for smaller files.  \
-                Given how expensive this operation can be, \"contents\" option is not shown in Interactive browse mode, but after a selection is made, can be utilized in Select mode.  \
-                The \"all\" option dumps all snapshot versions, and no attempt is made to determine if the file versions are distinct.")
+                or a user can simply update the modify time via 'touch'.  If only this flag is specified, the \"contents\" option compares the actual file contents of file versions, if their sizes match, \
+                and overrides the default \"metadata\" behavior.  The \"contents\" option can be expensive, as the file versions need to be read back and compared, and should probably only be used for smaller files.  \
+                Given how expensive this operation can be, for larger files or files with many versions, \"contents\" option is not shown in Interactive browse mode, \
+                but after a selection is made, can be utilized in Select or Restore modes.  The \"all\" or \"no-filter\" option dumps all snapshot versions, and no attempt is made to determine if the file versions are distinct.")
                 .display_order(9)
         )
         .arg(
@@ -618,7 +618,7 @@ impl Config {
         };
 
         let mut uniqueness = match matches.value_of("UNIQUENESS") {
-            Some("all") => ListSnapsOfType::All,
+            Some("all" | "no-filter") => ListSnapsOfType::All,
             Some("contents") => ListSnapsOfType::UniqueContents,
             Some("metadata" | _) | None => ListSnapsOfType::UniqueMetadata,
         };
