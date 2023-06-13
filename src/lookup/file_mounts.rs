b43f8493486a10fdfb9341d99bdc3fa4e8775dec
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-06-12 21:05:25 -0500

    Consolidate fn

--- src/lookup/file_mounts.rs
+++ src/lookup/file_mounts.rs
@@ -27,12 +27,12 @@ use crate::GLOBAL_CONFIG;
 
 #[derive(Debug)]
 pub struct MountsForFiles<'a> {
-    inner: BTreeMap<PathData, Vec<PathData>>,
+    inner: BTreeMap<&'a PathData, Vec<PathData>>,
     mount_display: &'a MountDisplay,
 }
 
 impl<'a> Deref for MountsForFiles<'a> {
-    type Target = BTreeMap<PathData, Vec<PathData>>;
+    type Target = BTreeMap<&'a PathData, Vec<PathData>>;
 
     fn deref(&self) -> &Self::Target {
         &self.inner
@@ -47,39 +47,26 @@ impl<'a> MountsForFiles<'a> {
     pub fn new(mount_display: &'a MountDisplay) -> Self {
         // we only check for phantom files in "mount for file" mode because
         // people should be able to search for deleted files in other modes
-        let (non_phantom_files, phantom_files): (Vec<PathData>, Vec<PathData>) = GLOBAL_CONFIG
+        let map: BTreeMap<&PathData, Vec<PathData>> = GLOBAL_CONFIG
             .paths
-            .clone()
-            .into_par_iter()
-            .partition(|pathdata| pathdata.metadata.is_some());
+            .par_iter()
+            .filter(|pathdata| {
+                if pathdata.metadata.is_none() {
+                    eprintln!("Error: Input file may not exist: {:?}", pathdata.path_buf);
+                    return false;
+                }
 
-        if !phantom_files.is_empty() {
-            eprintln!(
-                "Error: httm was unable to determine mount locations for all input files, \
-            because the following files do not appear to exist: "
-            );
-
-            phantom_files
-                .iter()
-                .for_each(|pathdata| eprintln!("{:?}", pathdata.path_buf));
-        }
-
-        MountsForFiles::from_raw_paths(&non_phantom_files, mount_display)
-    }
-
-    pub fn from_raw_paths(raw_vec: &[PathData], mount_display: &'a MountDisplay) -> Self {
-        let map: BTreeMap<PathData, Vec<PathData>> = raw_vec
-            .iter()
-            .flat_map(ProximateDatasetAndOptAlts::new)
+                true
+            })
+            .flat_map(|pathdata| ProximateDatasetAndOptAlts::new(&pathdata))
             .map(|prox_opt_alts| {
                 let vec = prox_opt_alts
                     .datasets_of_interest
                     .iter()
                     .map(PathData::from)
                     .collect();
-                (prox_opt_alts.pathdata.clone(), vec)
+                (prox_opt_alts.pathdata, vec)
             })
-            .rev()
             .collect();
 
         Self {
