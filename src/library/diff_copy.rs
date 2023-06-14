f2d81e691a0037972855abf38394b0db1927ea78
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-06-14 12:41:55 -0500

    Add comments

--- src/library/diff_copy.rs
+++ src/library/diff_copy.rs
@@ -29,14 +29,14 @@ use simd_adler32::Adler32;
 
 use crate::library::results::HttmResult;
 
-use super::results::HttmError;
-
 const CHUNK_SIZE: usize = 65_536;
 
 pub fn diff_copy(src: &Path, dst: &Path) -> HttmResult<()> {
+    // create source file reader
     let src_file = File::open(src)?;
     let mut src_reader = BufReader::with_capacity(CHUNK_SIZE, &src_file);
 
+    // create destination if it doesn't exist
     let dst_file = OpenOptions::new()
         .write(true)
         .read(true)
@@ -44,6 +44,10 @@ pub fn diff_copy(src: &Path, dst: &Path) -> HttmResult<()> {
         .open(dst)?;
     let src_len = src_file.metadata()?.len();
     dst_file.set_len(src_len)?;
+
+    // create destination file writer and maybe reader
+    // only include dest file reader if the dest file exists
+    // otherwise we just write to that location
     let mut opt_just_write = if dst.exists() {
         None
     } else {
@@ -52,46 +56,48 @@ pub fn diff_copy(src: &Path, dst: &Path) -> HttmResult<()> {
     };
     let mut dst_writer = BufWriter::with_capacity(CHUNK_SIZE, &dst_file);
 
+    // cur pos - byte offset in file,
     let mut cur_pos = 0u64;
+    // buffers are the reusable buffers for reads to, writes from src and dest
     let mut src_buffer = [0; CHUNK_SIZE];
     let mut dest_buffer = [0; CHUNK_SIZE];
 
     loop {
+        // read (size of buffer amt) from src, and dest if it exists
         let src_amt_read = src_reader.read(&mut src_buffer)?;
+
+        // if nothing left to read from file, break
         if src_amt_read == 0 {
             break;
         }
+
+        // read same amt from dest file, if it exists, to compare 
         if let Some(ref mut dst_reader) = opt_just_write {
             let _ = dst_reader.read(&mut dest_buffer)?;
         }
 
+        // write if dest doesn't exist or src, or if src and dest buffers do not match
         if opt_just_write.is_some() || !is_same_bytes(&src_buffer, &dest_buffer) {
+            // seek to current byte offset in dest writer
             let seek_pos = dst_writer.seek(SeekFrom::Start(cur_pos))?;
 
-            if seek_pos != cur_pos {
-                return Err(HttmError::new("Seek offset did not match requested offset.").into());
-            }
+            assert!(seek_pos == cur_pos);
 
-            let amt_written = if src_amt_read == CHUNK_SIZE {
-                dst_writer.write(&src_buffer)?
-            } else {
-                let range: &[u8] = &src_buffer[0..src_amt_read];
-                dst_writer.write(range)?
-            };
+            // write only amt read - imagine we read less than the amt of the buffer
+            // don't write past the end of the file with junk data at the end of the buffer
+            let range: &[u8] = &src_buffer[0..src_amt_read];
+            let amt_written = dst_writer.write(range)?;
 
-            if amt_written != src_amt_read {
-                return Err(HttmError::new(
-                    "Amount of bytes read did not match amount of bytes written.",
-                )
-                .into());
-            }
+            assert!(amt_written == src_amt_read);
 
+            // set current byte offset to either amt written or read if no write occurred
             cur_pos += amt_written as u64;
         } else {
             cur_pos += src_amt_read as u64;
         }
     }
 
+    // re docs, both a flush and a sync seem to be required re consistency
     dst_writer.flush()?;
     dst_file.sync_data()?;
 
