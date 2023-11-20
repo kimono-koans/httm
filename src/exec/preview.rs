8e863d24c3be4dcfcc539f73f3134c60ba31a5db
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-11-20 13:43:38 -0600

    Print error when snap_file var is empty

--- src/exec/preview.rs
+++ src/exec/preview.rs
@@ -22,44 +22,46 @@ use std::path::PathBuf;
 use which::which;
 
 pub struct PreviewSelection {
-  pub opt_preview_window: Option<String>,
-  pub opt_preview_command: Option<String>,
+    pub opt_preview_window: Option<String>,
+    pub opt_preview_command: Option<String>,
 }
 
 impl PreviewSelection {
-  pub fn new(view_mode: &ViewMode) -> HttmResult<Self> {
-    //let (opt_preview_window, opt_preview_command) =
-    let res = match &GLOBAL_CONFIG.opt_preview {
-      Some(defined_command) if matches!(view_mode, ViewMode::Select(_)) => {
-        let opt_live_version = if let ViewMode::Select(opt) = view_mode {
-          opt
-        } else {
-          unreachable!()
+    pub fn new(view_mode: &ViewMode) -> HttmResult<Self> {
+        //let (opt_preview_window, opt_preview_command) =
+        let res = match &GLOBAL_CONFIG.opt_preview {
+            Some(defined_command) if matches!(view_mode, ViewMode::Select(_)) => {
+                let opt_live_version = if let ViewMode::Select(opt) = view_mode {
+                    opt
+                } else {
+                    unreachable!()
+                };
+
+                let opt_preview_command = Some(Self::parse_preview_command(
+                    defined_command,
+                    opt_live_version,
+                )?);
+
+                PreviewSelection {
+                    opt_preview_window: Some("up:50%".to_owned()),
+                    opt_preview_command,
+                }
+            }
+            _ => PreviewSelection {
+                opt_preview_window: Some(String::new()),
+                opt_preview_command: None,
+            },
         };
 
-        PreviewSelection {
-          opt_preview_window: Some("up:50%".to_owned()),
-          opt_preview_command: Some(Self::parse_preview_command(
-            defined_command,
-            opt_live_version,
-          )?),
-        }
-      }
-      _ => PreviewSelection {
-        opt_preview_window: Some(String::new()),
-        opt_preview_command: None,
-      },
-    };
+        Ok(res)
+    }
 
-    Ok(res)
-  }
-
-  fn parse_preview_command(
-    defined_command: &str,
-    opt_live_version: &Option<String>,
-  ) -> HttmResult<String> {
-    let command = if defined_command == "default" {
-      match opt_live_version {
+    fn parse_preview_command(
+        defined_command: &str,
+        opt_live_version: &Option<String>,
+    ) -> HttmResult<String> {
+        let command = if defined_command == "default" {
+            match opt_live_version {
                 Some(live_version) if PathBuf::from(live_version).exists() && which("bowie").is_ok() => {
                     format!("bowie --direct \"$snap_file\" \"{live_version}\"")
                 },
@@ -73,22 +75,22 @@ impl PreviewSelection {
                     }
                 },
             }
-    } else {
-      match defined_command.split_ascii_whitespace().next() {
-        Some(potential_executable) => {
-          if which(potential_executable).is_err() {
-            return Err(HttmError::new("User specified a preview variable for a live version, but a live version for the file selected does not exist.").into());
-          }
-        }
-        None => {
-          return Err(
-            HttmError::new("httm could not determine a valid preview command from user's input.")
-              .into(),
-          );
-        }
-      }
+        } else {
+            match defined_command.split_ascii_whitespace().next() {
+                Some(potential_executable) => {
+                    if which(potential_executable).is_err() {
+                        return Err(HttmError::new("User specified a preview variable for a live version, but a live version for the file selected does not exist.").into());
+                    }
+                }
+                None => {
+                    return Err(HttmError::new(
+                        "httm could not determine a valid preview command from user's input.",
+                    )
+                    .into());
+                }
+            }
 
-      let parsed_command = match opt_live_version {
+            let parsed_command = match opt_live_version {
                 Some(live_version) if defined_command.contains("{live_file}") && !PathBuf::from(live_version).exists() => {
                     return Err(HttmError::new("User specified a preview variable for a live version, but a live version for the file selected does not exist.").into())
                 },
@@ -106,19 +108,19 @@ impl PreviewSelection {
                 },
             };
 
-      // protect ourselves from command like cat
-      // just waiting on stdin by appending the snap file
-      if parsed_command.contains("\"$snap_file\"") {
-        parsed_command
-      } else {
-        [defined_command, " \"$snap_file\""].into_iter().collect()
-      }
-    };
+            // protect ourselves from command like cat
+            // just waiting on stdin by appending the snap file
+            if parsed_command.contains("\"$snap_file\"") {
+                parsed_command
+            } else {
+                [defined_command, " \"$snap_file\""].into_iter().collect()
+            }
+        };
 
-    let res = match which("cut") {
+        let res = match which("cut") {
             Ok(_) => {
                 format!(
-                    "snap_file=\"`echo {{}} | cut -d'\"' -f2`\"; if test -f \"$snap_file\" || test -d \"$snap_file\" || test -L \"$snap_file\"; then exec 0<&-; {command} 2>&1; fi"
+                    "snap_file=\"`echo {{}} | cut -d'\"' -f2`\"; if test -f \"$snap_file\" || test -d \"$snap_file\" || test -L \"$snap_file\"; then exec 0<&-; {command} 2>&1; else printf \"Required parameter snap_file is not found or is invalid.\"; fi"
                 )
             }
             Err(_) => {
@@ -128,6 +130,6 @@ impl PreviewSelection {
             }
         };
 
-    Ok(res)
-  }
+        Ok(res)
+    }
 }
