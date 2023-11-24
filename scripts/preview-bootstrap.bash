6bb695e2a8a5d21bc2c0655a25f3822f7ab6c79f
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-11-23 21:11:36 -0600

    Update preview update script

--- scripts/preview-bootstrap.bash
+++ scripts/preview-bootstrap.bash
@@ -29,6 +29,15 @@ print_err() {
 	printf "%s\n" "ERROR: $*" 1>&2
 }
 
+print_warn_exit() {
+	print_warn "$@"
+	exit 0
+}
+
+print_warn() {
+	printf "%s\n" "WARN: $*"
+}
+
 graceful_shutdown() {
 	printf "%s\n" "--"
 	exit 0
@@ -36,21 +45,9 @@ graceful_shutdown() {
 
 prep_exec() {
 	[[ -n "$(
-		command -v bash
-		exit 0
-	)" ]] || print_err_exit "'bash' is required to execute 'httm --preview'.  Please check that 'bash' is in your path."
-	[[ -n "$(
 		command -v cut
 		exit 0
 	)" ]] || print_err_exit "'cut' is required to execute 'httm --preview'.  Please check that 'cut' is in your path."
-}
-
-is_fancy_border_line() {
-	local raw_input=""
-	raw_input=$1
-
-	# is border line? is not a well formed line of text, probably a border line
-	[[ "$( echo $raw_input | grep -c '"' )" -gt 0 ]] || graceful_shutdown
 }
 
 bootstrap_preview() {
@@ -63,16 +60,15 @@ bootstrap_preview() {
 
 	[[ -n $raw_input ]] || print_err_exit "Selection is empty."
 
-	# check does the string contain any quotes
-	is_fancy_border_line $raw_input
+	[[ $raw_input != ─*─ ]] || graceful_shutdown
 
-	# remove first and last chars in string in case they are also quotes
+	# remove first and last chars in string in case they are also quotes 
 	# possible we drop good chars, but these chars are unnecessary for parsing
-	snap_file="$(echo ${raw_input:1:-1} | cut -d'"' -f2)"
+	snap_file="$(echo ${raw_input} | cut -d'"' -f2)"
 
-	[[ -n "$snap_file" ]] || print_err_exit "Path is empty."
+	[[ -n "$snap_file" ]] || print_err_exit "Snap file path is empty."
 
-	[[ -f "$snap_file" ]] || [[ -d "$snap_file" ]] || [[ -L "$snap_file" ]] || print_err_exit "Path does not refer to a valid file, link or directory." 
+	[[ -f "$snap_file" ]] || [[ -d "$snap_file" ]] || [[ -L "$snap_file" ]] || print_warn_exit "Selection does not refer to a valid file, link or directory."
 
 	exec 0<&-
 	{command} 2>&1
