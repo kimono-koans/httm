7c1baff0030bfa428d893107ea2c4245e3f07a36
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-12-08 19:11:57 -0600

    No need to lower priority of background search

--- src/exec/recursive.rs
+++ src/exec/recursive.rs
@@ -95,20 +95,6 @@ impl RecursiveMainLoop {
         skim_tx: &SkimItemSender,
         hangup_rx: &Receiver<Never>,
     ) -> HttmResult<()> {
-        #[cfg(feature = "setpriority")]
-        #[cfg(target_os = "linux")]
-        #[cfg(target_env = "gnu")]
-        {
-            use crate::library::utility::ThreadPriorityType;
-
-            let tid = std::process::id();
-            if !matches!(
-                GLOBAL_CONFIG.exec_mode,
-                ExecMode::NonInteractiveRecursive(_)
-            ) {
-                let _ = ThreadPriorityType::Process.nice_thread(Some(tid), 1i32);
-            }
-        }
         // runs once for non-recursive but also "primes the pump"
         // for recursive to have items available, also only place an
         // error can stop execution
