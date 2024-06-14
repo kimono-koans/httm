d37a66f1ec0cba2f6712c1bcf12272e4d7128cb2
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2024-06-14 01:24:24 -0500

    Cleanup

--- src/parse/snaps.rs
+++ src/parse/snaps.rs
@@ -81,7 +81,7 @@ impl MapOfSnaps {
 
                             NOTICE_FALLBACK.call_once(|| {
                                 eprintln!(
-                                    "NOTICE: Falling back to detection of btrfs mounts defined by Snapper.",
+                                    "NOTICE: Falling back to detection of btrfs mounts perhaps defined by Snapper.",
                                 );
                             });
 
