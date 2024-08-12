4a234577af519bb538b1a9d23b5238ffa01563b7
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2024-08-12 13:18:34 -0500

    List additional caveats

--- src/config/generate.rs
+++ src/config/generate.rs
@@ -316,7 +316,11 @@ fn parse_args() -> ArgMatches {
                 httm will copy only files and their attributes that have changed since a specified snapshot, from that snapshot, to its live dataset. \
                 httm will also take two precautionary snapshots, one before and one after the copy. \
                 Should the roll forward fail for any reason, httm will roll back to the pre-execution state. \
-                Caveats: This is a ZFS only option which requires super user privileges.")
+                Caveats: This is a ZFS only option which requires super user privileges.  \
+                Not all filesystem features are supported (for instance, Solaris door or sockets on the snapshot) and will cause a roll forward to fail.  \
+                Certain special/files objects will be copied or recreated, but are not guaranteed to be in the same state as the snapshot (for instance, fifos).\
+                ZFS block clone copying of so many files in parallel can cause a kernel crash on some configurations.  \
+                The safest method is to pass this argument with the NO_CLONES argument.")
                 .conflicts_with_all(&["BROWSE", "RESTORE", "ALT_REPLICATED", "REMOTE_DIR", "LOCAL_DIR"])
                 .display_order(13)
                 .action(ArgAction::Append)
