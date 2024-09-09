abc62bb835dd9bdc6c24db6eed299b79f92b8f11
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2024-09-09 10:17:50 -0500

    Fix alias map

--- src/parse/aliases.rs
+++ src/parse/aliases.rs
@@ -24,23 +24,23 @@ use std::sync::Arc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RemotePathAndFsType {
-    pub remote_dir: Box<Path>,
+    pub remote_dir: Arc<Path>,
     pub fs_type: FilesystemType,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MapOfAliases {
-    inner: BTreeMap<Arc<Path>, RemotePathAndFsType>,
+    inner: BTreeMap<Box<Path>, RemotePathAndFsType>,
 }
 
-impl From<BTreeMap<Arc<Path>, RemotePathAndFsType>> for MapOfAliases {
-    fn from(map: BTreeMap<Arc<Path>, RemotePathAndFsType>) -> Self {
+impl From<BTreeMap<Box<Path>, RemotePathAndFsType>> for MapOfAliases {
+    fn from(map: BTreeMap<Box<Path>, RemotePathAndFsType>) -> Self {
         Self { inner: map }
     }
 }
 
 impl Deref for MapOfAliases {
-    type Target = BTreeMap<Arc<Path>, RemotePathAndFsType>;
+    type Target = BTreeMap<Box<Path>, RemotePathAndFsType>;
 
     fn deref(&self) -> &Self::Target {
         &self.inner
@@ -55,10 +55,6 @@ impl MapOfAliases {
         opt_local_dir: Option<String>,
         pwd: &Path,
     ) -> HttmResult<Option<MapOfAliases>> {
-        if opt_raw_aliases.is_none() && opt_remote_dir.is_none() && opt_local_dir.is_none() {
-            return Ok(None);
-        }
-
         let alias_values: Option<Vec<String>> = match std::env::var_os("HTTM_MAP_ALIASES") {
             Some(env_map_alias) => Some(
                 env_map_alias
@@ -79,89 +75,88 @@ impl MapOfAliases {
             std::env::var_os("HTTM_SNAP_POINT").map(|s| Box::from(Path::new(&s)))
         };
 
-        if opt_snap_dir.is_some() || alias_values.is_some() {
-            let env_local_dir: Option<Box<Path>> =
-                std::env::var_os("HTTM_LOCAL_DIR").map(|s| Box::from(Path::new(&s)));
+        let env_local_dir: Option<Box<Path>> =
+            std::env::var_os("HTTM_LOCAL_DIR").map(|s| Box::from(Path::new(&s)));
 
-            let opt_local_dir: Option<Box<Path>> = if let Some(value) = opt_local_dir {
-                Some(Box::from(Path::new(&value)))
-            } else {
-                env_local_dir
-            };
+        if alias_values.is_none() && opt_snap_dir.is_none() {
+            return Ok(None);
+        }
 
-            // user defined dir exists?: check that path contains the hidden snapshot directory
-            let snap_point = opt_snap_dir.map(|snap_dir| {
-                // local relative dir can be set at cmdline or as an env var,
-                // but defaults to current working directory if empty
-                let local_dir = opt_local_dir.unwrap_or_else(|| pwd.into());
-
-                (snap_dir, local_dir)
-            });
-
-            let mut aliases_iter: Vec<(Box<Path>, Box<Path>)> = match alias_values {
-                Some(input_aliases) => {
-                    let res: Option<Vec<(Box<Path>, Box<Path>)>> = input_aliases
-                        .iter()
-                        .map(|alias| {
-                            alias.split_once(':').map(|(first, rest)| {
-                                (Path::new(first).into(), Path::new(rest).into())
-                            })
-                        })
-                        .collect();
-
-                    res.ok_or_else(|| {
-                        HttmError::new(
-                            "Must use specified delimiter (':') between aliases for MAP_ALIASES.",
-                        )
-                    })?
-                }
-                None => Vec::new(),
-            };
-
-            if let Some(value) = snap_point {
-                aliases_iter.push(value)
-            }
-
-            let map_of_aliases: BTreeMap<Arc<Path>, RemotePathAndFsType> = aliases_iter
-                .into_iter()
-                .filter_map(|(local_dir, snap_dir)| {
-                    match map_of_datasets
-                        .get_key_value(local_dir.as_ref())
-                        .map(|(k, _v)| (k.clone(), snap_dir)) {
-                            Some((k, snap_dir)) => {
-                                if !snap_dir.exists() {
-                                    eprintln!("WARN: An alias path specified does not exist, or is not mounted: {:?}", snap_dir);
-                                    return None
-                                }
-
-                                Some((k.clone(), snap_dir))
-                            }
-                            None => {
-                                eprintln!("WARN: An alias path specified does not exist, or is not mounted: {:?}", local_dir);
-                                None
-                            }
-                        }
-                })
-                .filter_map(|(local_dir, remote_dir)| {
-                    FilesystemType::new(&remote_dir).map(|fs_type| {
-                        (
-                            local_dir,
-                            RemotePathAndFsType {
-                                remote_dir,
-                                fs_type,
-                            },
-                        )
-                    })
-                })
-                .collect();
-
-            if map_of_aliases.is_empty() {
-                return Ok(None);
-            }
-
-            return Ok(Some(map_of_aliases.into()));
+        let opt_local_dir: Option<Box<Path>> = if let Some(value) = opt_local_dir {
+            Some(Box::from(Path::new(&value)))
+        } else {
+            env_local_dir
         };
 
-        return Ok(None);
+        // user defined dir exists?: check that path contains the hidden snapshot directory
+        let snap_point = opt_snap_dir.map(|snap_dir| {
+            // local relative dir can be set at cmdline or as an env var,
+            // but defaults to current working directory if empty
+            let local_dir = opt_local_dir.unwrap_or_else(|| pwd.into());
+
+            (snap_dir, local_dir)
+        });
+
+        let mut aliases_iter: Vec<(Box<Path>, Box<Path>)> = match alias_values {
+            Some(input_aliases) => {
+                let res: Option<Vec<(Box<Path>, Box<Path>)>> = input_aliases
+                    .iter()
+                    .map(|alias| {
+                        alias
+                            .split_once(':')
+                            .map(|(first, rest)| (Path::new(first).into(), Path::new(rest).into()))
+                    })
+                    .collect();
+
+                res.ok_or_else(|| {
+                    HttmError::new(
+                        "Must use specified delimiter (':') between aliases for MAP_ALIASES.",
+                    )
+                })?
+            }
+            None => Vec::new(),
+        };
+
+        if let Some(value) = snap_point {
+            aliases_iter.push(value)
+        }
+
+        let map_of_aliases: BTreeMap<Box<Path>, RemotePathAndFsType> = aliases_iter
+            .into_iter()
+            .filter_map(|(local_dir, snap_dir)| {
+                match map_of_datasets
+                    .get_key_value(snap_dir.as_ref()).map(|(k, _v)| k.clone()) {
+                        Some(snap_dir) => {
+                            if !snap_dir.exists() {
+                                eprintln!("WARN: An alias path specified does not exist, or is not mounted: {:?}", snap_dir);
+                                return None
+                            }
+
+                            Some((local_dir, snap_dir))
+                        }
+                        None => {
+                            eprintln!("WARN: An alias path specified does not exist, or is not mounted: {:?}", local_dir);
+                            None
+                        }
+                    }
+            })
+            .filter_map(|(local_dir, remote_dir)| {
+                FilesystemType::new(&remote_dir).map(|fs_type| {
+                    (
+                        local_dir,
+                        RemotePathAndFsType {
+                            remote_dir,
+                            fs_type,
+                        },
+                    )
+                })
+            })
+            .collect();
+
+        if map_of_aliases.is_empty() {
+            return Ok(None);
+        }
+
+        Ok(Some(map_of_aliases.into()))
     }
 }
