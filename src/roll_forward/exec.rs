5c8e09a1504c1a7ea16bf718df498fe66de148c5
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2025-01-25 00:30:37 -0600

    Revert "Cleanup"
    This reverts commit 7c8541a6be150da5ad094385d05d842cf0ba37de.

--- src/roll_forward/exec.rs
+++ src/roll_forward/exec.rs
@@ -232,6 +232,16 @@ impl RollForward {
             .try_for_each(|(snap_path, live_path)| {
                 self.progress_bar.tick();
 
+                // zfs diff sometimes doesn't pick up rename events
+                // here we cleanup
+                if snap_path.try_exists()? && !live_path.try_exists()? {
+                    Copy::direct_quiet(&snap_path, &live_path, true)?
+                }
+
+                if GLOBAL_CONFIG.opt_debug {
+                    DiffCopy::confirm(&snap_path, &live_path)?
+                }
+
                 is_metadata_same(&snap_path, &live_path)
             })?;
 
@@ -254,7 +264,17 @@ impl RollForward {
             .try_for_each(|(snap_path, live_path)| {
                 self.progress_bar.tick();
 
-                Preserve::direct(&snap_path, &live_path)?;
+                // zfs diff sometimes doesn't pick up rename events
+                // here we cleanup
+                if snap_path.try_exists()? && !live_path.try_exists()? {
+                    Copy::direct_quiet(&snap_path, &live_path, true)?
+                } else {
+                    Preserve::direct(&snap_path, &live_path)?;
+                }
+
+                if GLOBAL_CONFIG.opt_debug {
+                    DiffCopy::confirm(&snap_path, &live_path)?
+                }
 
                 is_metadata_same(&snap_path, &live_path)
             })?;
